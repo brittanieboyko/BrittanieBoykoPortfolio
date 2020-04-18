ccc399fcbe83e8b688235702bd39d36133c27309
brittanieb <brittanie.boyko@gmail.com>
2020-04-18 16:49:41 -0400

    deleted unused css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
 import About from "./components/pages/About";
 import Contact from "./components/pages/Contact";
-import './App.css';
 
 function App() {
   return (
@@ -13,6 +13,7 @@ function App() {
       <Route exact path="/" component={About} />
       <Route exact path="/about" component={About} />
       <Route exact path="/contact" component={Contact} />
+      <Footer />
     </div>
   </Router>
   );
