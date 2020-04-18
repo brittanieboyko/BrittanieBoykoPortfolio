48d017ae9a9f9d9b66297347bf2de530f9e8b5f6
brittanieb <brittanie.boyko@gmail.com>
2020-04-18 16:16:47 -0400

    added bootstrap. Implemented and styled responsive navbar

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,34 +1,40 @@
 import React from "react";
-import { Link, useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
 import "./style.css";
 
 function NavBar() {
-  const location = useLocation();
-
   return (
-    <ul className="nav nav-tabs">
-      <li className="nav-item">
-        <Link to="/" className={location.pathname === "/" ? "nav-link active" : "nav-link"}>
-          Home
-        </Link>
-      </li>
-      <li className="nav-item">
-        <Link
-          to="/about"
-          className={location.pathname === "/about" ? "nav-link active" : "nav-link"}
-        >
-          About
-        </Link>
-      </li>
-      <li className="nav-item">
-        <Link
-          to="/contact"
-          className={location.pathname === "/contact" ? "nav-link active" : "nav-link"}
-        >
-          Contact
-        </Link>
-      </li>
-    </ul>
+    <nav
+      className="navbar navbar-expand-lg navbar-light custom-nav"
+      id="bootstrap-overrides"
+    >
+      <Link to="/" className=" navbar-brand text-uppercase" id="myName">
+        Brittanie Boyko
+      </Link>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNavAltMarkup"
+        aria-controls="navbarNavAltMarkup"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div className="navbar-nav">
+          <Link to="/about" className="nav-item nav-link">
+            About
+          </Link>
+          <Link to="/contact" className="nav-item nav-link">
+            Contact
+          </Link>
+        </div>
+      </div>
+    </nav>
+
+    
   );
 }
 
