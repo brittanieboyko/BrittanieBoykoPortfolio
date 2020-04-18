94af33e80544d1ffd628ae90db22c73011869010
brittanieb <brittanie.boyko@gmail.com>
2020-04-18 17:56:55 -0400

    created and styled contact form component - minimal functionality

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,11 +1,45 @@
-import React from "react";
+import React, { Component } from "react";
+import ContactForm from "../ContactForm";
 
-function Contact() {
-  return (
-    <div>
-      <p> Sup, again</p>
-    </div>
-  );
+class Contact extends Component {
+  state = {
+    name: "",
+    email: "",
+    message: "",
+  };
+
+  handleInputChange = (event) => {
+    let value = event.target.value;
+    const name = event.target.name;
+  
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  handleFormSubmit = (event) => {
+    event.preventDefault()
+    console.log(this.state.name, this.state.email, this.state.message)
+    
+
+    this.setState({
+      firstName: "",
+      lastName: "",
+      message: "",
+    });
+  };
+
+  render() {
+    return (
+      <ContactForm
+        onChange ={this.handleInputChange}
+        onClick={this.handleFormSubmit}
+        name={this.state.name}
+        email={this.state.email}
+        message={this.state.message}
+      />
+    );
+  }
 }
 
 export default Contact;
