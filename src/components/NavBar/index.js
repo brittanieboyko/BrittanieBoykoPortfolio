6d6c20ccfb5487c99739fddc085665971cd56936
brittanieb <brittanie.boyko@gmail.com>
2020-07-27 17:44:10 -0400

    removed toggle option on navbar

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -4,32 +4,24 @@ import "./style.css";
 
 function NavBar() {
   return (
+    <>
     <nav
       className="navbar navbar-expand-lg navbar-light custom-nav"
       id="bootstrap-overrides"
     >
-      <Link to="/" className=" navbar-brand text-uppercase" id="myName">
+      <Link to="/" className="navbar-brand text-uppercase mx-auto" id="myName">
         Brittanie Boyko
       </Link>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarNavAltMarkup"
-        aria-controls="navbarNavAltMarkup"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div className="navbar-nav">
-
-        </div>
-      </div>
-    </nav>
-
-    
+      </nav>
+      <nav class="nav">
+        <Link to="/about" className="nav-link mx-auto">
+            About
+        </Link>
+        <Link to="/contact" className="nav-link mx-auto">
+          Work
+        </Link>
+      </nav>
+    </>
   );
 }
 
