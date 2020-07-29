beb23f1da582cebb631b955255eb130de4ab71bc
brittanieb <brittanie.boyko@gmail.com>
2020-07-29 16:49:33 -0400

    odded overlay on project images

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -2,22 +2,38 @@ import React from "react";
 import "./style.css";
 
 function Project(props) {
-    return (
-        <div className="container">
-            <div className="row">
-                {props.projects.map(project => (
-                    <div className="card col-lg-4 image-spacing flex-column d-flex text-center custom-card" key={project.id} >
-                    <div className="img-fluid img" style={{backgroundImage: `url(${(project.image)})`}}></div>
-                    <div className="card-body">
-                        <h5 className="card-title">{project.name}</h5>
-                        <a href={project.project} className="btn btn-primary custom-button mt-auto">Project</a>
-                        <a href={project.repo}className="btn btn-primary custom-button mt-auto">Repo</a>
-                    </div>
-                </div>
-                ))}
+  return (
+    <div className="container">
+      <div className="row">
+        {props.projects.map((project) => (
+          <div
+            className="card col-lg-4 col-md-4 col-sm-6 col-xs-12 image-spacing flex-column d-flex text-center custom-card"
+            key={project.id}
+          >
+            <div className="hovereffect">
+              <div
+                className="img-fluid img img-responsive"
+                style={{ backgroundImage: `url(${project.image})` }}
+              ></div>
+              <div className="overlay">
+                <h2 className="card-title">{project.name}</h2>
+                <p>
+                  <a href={project.project} className="custom-button mt-auto">
+                    Project
+                  </a>
+                </p>
+                <p>
+                  <a href={project.repo} className="custom-button mt-auto">
+                    Repo
+                  </a>
+                </p>
+              </div>
             </div>
-        </div>
-    )
-};
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
 
-export default Project
+export default Project;
