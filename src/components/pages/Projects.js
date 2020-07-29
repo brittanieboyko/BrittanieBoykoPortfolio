aefdd9908411a57e216e1e3d2b14fb5d7d7a60b3
brittanieb <brittanie.boyko@gmail.com>
2020-07-28 21:37:45 -0400

    created projects page

---
+++ src/components/pages/Projects.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Project from "../Project";
+import projects from "../../assets/projects";
+
+
+function Projects() {
+  return (
+    <>
+      <Project projects={projects}/>
+    </>
+  );
+}
+
+export default Projects;
